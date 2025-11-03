4ede060a63c1ce720d7b247768a31a2acdf03087
Insearchman <insearchman@gmail.com>
2025-11-03 20:11:12 +0300

    [MainMenu] X sound

--- Assets/_Project/Scripts/MainMenu/ExitButton.cs
+++ Assets/_Project/Scripts/MainMenu/ExitButton.cs
@@ -1,9 +1,11 @@
+using FishingHim.Common;
 using UnityEngine;
 
 public class ExitButton : MonoBehaviour
 {
     public void QuitGame()
     {
+        SoundsManager.Instance.PlaySound(SoundType.MainClick);
         Application.Quit();
     }
 }
